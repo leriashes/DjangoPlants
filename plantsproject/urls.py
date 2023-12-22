b705b3a4bde19a4da5581d4fe688dfbc20ba705a
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-12-22 16:26:28 +0700

    Администрирование

--- plantsproject/urls.py
+++ plantsproject/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     #path('account/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
